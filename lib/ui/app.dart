47b577e41deaf19566d31f4085db1f07e0b00442
dmytroplashkin <Dmitriycustom33>
2022-09-20 16:28:31 +0300

    added reflow screen

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -6,10 +6,13 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Dating App',
-      home: WelcomeScreen(),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Dating App',
+        home: WelcomeScreen(),
+      ),
     );
   }
 }
